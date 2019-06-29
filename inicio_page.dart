128fea646d15324e6626b6fd5c78f03a7a2e8b51
xcheko51x <xcheko51x@gmail.com>
2019-06-29 14:29:24 -0500

    Add files via upload

---
+++ inicio_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InicioPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pagina de inicio"),
+      ),
+      body: Center(
+        child: Text("HOLA A TODOS"),
+      ),
+    );
+  }
+  
+}
---
+++ main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'inicio_page.dart';
+import 'dart:async';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: SplashScreen(),
+    )
+  );
+}
+
+class SplashScreen extends StatefulWidget {
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    Future.delayed(
+      Duration(seconds: 5),
+        () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => InicioPage()
+            ),
+        );
+        }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Image.asset("assets/dispositivos.gif"),
+      ),
+    );
+  }
+
+
+
+}
